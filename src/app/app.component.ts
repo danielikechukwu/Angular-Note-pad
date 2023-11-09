3e77f817a9c743ad182640e52d28eca634f04bb0
Daniel ikechukwu <daniel.ikechukwu@avosoft.co.uk>
2023-11-09 15:45:04 +0000

    Notepad header component was included

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 interface IHusband {
   name: string,
@@ -11,7 +12,7 @@ interface IHusband {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
